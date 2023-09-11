311cb9f3953f63192cd001a00ace64e2f305ca38
nazala-mafa <thenazala@gmail.com>
2023-09-11 11:18:57 +0700

    fix prod

--- database/migrations/2023_09_09_093954_transaction_tables.php
+++ database/migrations/2023_09_09_093954_transaction_tables.php
@@ -6,28 +6,33 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
     public function up(): void
     {
-        Schema::create('transaction_header', function(Blueprint $table) {
+        Schema::create('transaction_header', function (Blueprint $table) {
             $table->id();
             $table->string('description');
-            // $table->enum('code', []);
+            /**
+             * #catatan
+             * Pada Diagram ERD di pdf petunjuk pengerjaan seharusnya menggunakan enum,
+             * karena tidak memungkinkan menggunakan enum untuk data yang tidak sama dengan yang disediakan di dalam enum
+             * maka saya terpaksa menggunakan varchar
+             */
+            // $table->enum('code', [123456, 1234]);
             $table->string('code')->unique();
             $table->double('rate_euro');
             $table->dateTime('date_paid');
         });
 
-        Schema::create('ms_category', function(Blueprint $table) {
+        Schema::create('ms_category', function (Blueprint $table) {
             $table->id();
             $table->string('name');
         });
 
-        Schema::create('transaction_detail', function(Blueprint $table) {
+        Schema::create('transaction_detail', function (Blueprint $table) {
             $table->id();
             $table->foreignIdFor(TransactionHeader::class, 'transaction_id')->index()->references('id')->on('transaction_header')->cascadeOnDelete()->restrictOnUpdate();
             $table->foreignIdFor(TransactionCategory::class, 'transaction_category_id')->index()->references('id')->on('ms_category')->cascadeOnDelete()->restrictOnUpdate();
